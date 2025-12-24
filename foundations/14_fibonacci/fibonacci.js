f0e235249bb9a80a201d7e7fcac369b85082dd38
Jugal Balaji <jugalbalaji97@gmail.com>
2025-12-24 22:25:18 +0530

    complete 14_fibonacci exercise in foundations

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n, sum=1, prev=0) {
+    n = parseInt(n);
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n == 1) return sum;
+    else {
+        [prev, sum] = [sum, sum + prev];
+        return fibonacci(n-1, sum, prev);
+    }
 };
 
 // Do not edit below this line
