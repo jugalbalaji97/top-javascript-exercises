8ebb199d7b581367fef99d0e620f9b72faec4341
Jugal Balaji <jugalbalaji97@gmail.com>
2025-12-24 21:43:40 +0530

    complete 13_palindromes exercise in foundations

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+function cleanString(str) {
+    return str.replace(/[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase();
+}
 
+const palindromes = function (string) {
+    string = cleanString(string);
+    if(string.length == 0 || string.length == 1) return true;
+    else {
+        if (string[0] == string[string.length - 1]) {
+            string = string.slice(1, string.length - 1);
+            return palindromes(string);
+        }
+        else return false;
+    }
 };
+
+console.log(palindromes("racecara"));
 
 // Do not edit below this line
 module.exports = palindromes;
