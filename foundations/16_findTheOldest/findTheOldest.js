05a66fa1f2c800064e34583297ec8176bca639ce
Jugal Balaji <jugalbalaji97@gmail.com>
2025-12-25 00:16:15 +0530

    complete 15_getTheTitles and 16_findTheOldest exercises in foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((personA, personB) => {
+        if (!("yearOfDeath" in personA)) personA.yearOfDeath = new Date().getFullYear();
+        if (!("yearOfDeath" in personB)) personB.yearOfDeath = new Date().getFullYear();
+        return (personB.yearOfDeath - personB.yearOfBirth) - (personA.yearOfDeath - personA.yearOfBirth)
+    })[0];
 };
 
 // Do not edit below this line
